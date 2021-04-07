b08cbc4bcf5f4656bcfdc371c86ad8f9f4262cf1
Danar Saady <saady.danar@outlook.de>
2021-04-07 20:52:03 +0200

    add model and application properties

---
+++ src/main/java/com/model/Todo.java
@@ -0,0 +1,47 @@
+package com.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String description;
+    private Priority priority;
+
+    public Todo(long id, String description, Priority priority){
+        this.id=id;
+        this.description=description;
+        this.priority=priority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+}

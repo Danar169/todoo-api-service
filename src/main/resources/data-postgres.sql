1e14611ab9cf5e1e62ed7523699943e4a0155d2e
Danar Saady <saady.danar@outlook.de>
2021-04-08 19:54:32 +0200

    sql scripts and edit properties file

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,7 @@
+INSERT INTO todos(description, priority) VALUES('Einkaufen gehen', 3);
+INSERT INTO todos(description, priority) VALUES('Eltern besuchen', 1);
+INSERT INTO todos(description, priority) VALUES('lernen', 2);
+INSERT INTO todos(description, priority) VALUES('Als ich gestern früh aufwachte, bemerkte ich, dass ich wach wurde.', 3);
+INSERT INTO todos(description, priority) VALUES('duschen', 2);
+INSERT INTO todos(description, priority) VALUES('putzen', 3);
+INSERT INTO todos(description, priority) VALUES('kochen', 1);
